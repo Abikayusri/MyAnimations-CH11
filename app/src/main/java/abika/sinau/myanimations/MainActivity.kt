14d663001bc6b87fa783f62e5eec60474b9a71f6
Abika Chairul Yusri <35727186+Abikayusri@users.noreply.github.com>
2020-07-25 01:23:26 +0700

    Finish materi animation transition

--- app/src/main/java/abika/sinau/myanimations/MainActivity.kt
+++ app/src/main/java/abika/sinau/myanimations/MainActivity.kt
@@ -9,33 +9,9 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    //TextView
-    lateinit var txtMessage: TextView
-
-    //Button
-    lateinit var btn: Button
-
-    // Animation
-    lateinit var animFadein: Animation
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        txtMessage = findViewById(R.id.text)
-        btn = findViewById(R.id.btn)
-        //loading Animation
-        animFadein = AnimationUtils.loadAnimation(
-            applicationContext,
-            R.anim.fade_in
-        )
-
-        //handling aniamtion on button click
-        btn.setOnClickListener {
-            txtMessage.visibility = View.VISIBLE
-            // starting the animation
-            txtMessage.startAnimation(animFadein)
-        }
     }
 }
