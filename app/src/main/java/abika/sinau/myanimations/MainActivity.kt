f74e12c950376d859c6eab4624fbd8b8e9625796
Abika Chairul Yusri <35727186+Abikayusri@users.noreply.github.com>
2020-07-24 13:44:22 +0700

    MyAnimations Chapter 11 - Animations

---
+++ app/src/main/java/abika/sinau/myanimations/MainActivity.kt
@@ -0,0 +1,40 @@
+package abika.sinau.myanimations
+
+import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    //TextView
+    lateinit var txtMessage: TextView
+
+    //Button
+    lateinit var btn: Button
+
+    // Animation
+    lateinit var animFadein: Animation
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        txtMessage = findViewById<TextView>(R.id.text)
+        btn = findViewById<Button>(R.id.btn)
+        //loading Animation
+        animFadein = AnimationUtils.loadAnimation(
+                applicationContext,
+                R.anim.fade_in)
+
+        //handling aniamtion on button click
+        btn.setOnClickListener {
+            txtMessage.visibility = View.VISIBLE
+            // starting the animation
+            txtMessage.startAnimation(animFadein)
+        }
+    }
+}
